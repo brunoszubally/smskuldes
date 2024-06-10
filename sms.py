2251f862ebb95acd354753673864fdd60a62e9e7
Szubally Bruno <szubally.bruno@epistogram.com>
2024-06-10 08:15:46 +0200

    updated auth

--- sms.py
+++ sms.py
@@ -4,90 +4,65 @@ import urllib.parse
 import subprocess
 from dotenv import load_dotenv
 import os
+import streamlit_authenticator as stauth
+import pickle
+from pathlib import Path
 
 # Load environment variables
 load_dotenv()
 
-# Get user credentials and curl key from environment variables
-USER_CREDENTIALS = {
-    os.getenv("USER1"): os.getenv("PASSWORD1"),
-    os.getenv("USER2"): os.getenv("PASSWORD2")
-}
 
 CURL_KEY = os.getenv("CURL_KEY")
 
-# Function to verify user login
-def login(username, password):
-    if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
-        return True
-    return False
 
-# Function to extract the second part of the name, handling empty cells
-def get_first_name(full_name):
-    if pd.notna(full_name):
-        parts = full_name.split()
-        if len(parts) > 1:
-            return parts[1]
-    return ""
+# Load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
 
-# Function to convert phone number to required format, handling empty cells
-def convert_phone_number(phone):
-    if pd.notna(phone):
-        phone = str(phone).split('.')[0]  # Remove any decimal points
-        if phone.startswith('06'):
-            return phone.replace('06', '+36', 1)
-        elif phone.startswith('6'):
-            return '+36' + phone[1:]
-    return ""
+names = ["Bruno Szubally", "kristof Szentes"]
+usernames = ["bruno.szubally", "kristof.szentes"]
 
-# Function to format the datetime, handling invalid or empty values
-def format_datetime(dt):
-    try:
-        return pd.to_datetime(dt).strftime('%H:%M')
-    except ValueError:
-        return ""
+credentials = {
+    "usernames": {
+        usernames[0]: {"name": names[0], "password": hashed_passwords[0]},
+        usernames[1]: {"name": names[1], "password": hashed_passwords[1]},
+    }
+}
 
-# Function to construct the curl command with URL encoding
-def construct_curl_command(row, message_template):
-    if message_template == 'donation':
-        message = f"{row['first_name']}, ne felejtsd, hogy a holnapi nap várunk téged vérplazma donációra az OkosPlazmába, {row['formatted_time']}-ra! Mátészalka, Bajcsy-Zsilinszky u. 17."
-    else:
-        message = f"{row['first_name']}, holnap várunk alkalmassági vizsgálatra az OkosPlazmába {row['formatted_time']}-ra, és 5.000 Ft-ot adunk a sikeres vizsgálatért! Bajcsy-Zsilinszky u. 17."
-    
-    encoded_message = urllib.parse.quote(message)
-    return f'curl -s "https://seeme.hu/gateway?key={CURL_KEY}&message={encoded_message}&number={row["formatted_phone"]}&callback=4,6,7&format=json"'
+authenticator = stauth.Authenticate(
+    credentials,
+    "flowergpt",
+    "abcdef",
+    cookie_expiry_days=30
+)
 
-# Streamlit app
-st.title("SMS Küldés Vérplazma Donációhoz")
+name, authentication_status, username = authenticator.login('main', fields={
+    'Form name': 'Belépés', 'Username': 'Felhasználónév', 'Password': 'Jelszó', 'Login': 'Belépés'})
 
-css = '''
-<style>
-[data-testid="stFileUploaderDropzone"] div div::before { content:"Tallózd be az Excel fájlt!"}
-[data-testid="stFileUploaderDropzone"] div div span{display:none;}
-[data-testid="stFileUploaderDropzone"] div div::after { content:""}
-[data-testid="stFileUploaderDropzone"] div div small{display:none;}
-[data-testid="stFileUploaderDropzone"] button{display:none;}
-</style>
-'''
-st.markdown(css, unsafe_allow_html=True)
+if authentication_status == False:
+    st.error("Username/password is incorrect")
 
-if 'logged_in' not in st.session_state:
-    st.session_state.logged_in = False
+if authentication_status == None:
+    st.warning("Add meg az adataidat!")
 
-if not st.session_state.logged_in:
-    with st.form("login_form"):
-        username = st.text_input("Felhasználónév")
-        password = st.text_input("Jelszó", type="password")
-        submit_button = st.form_submit_button("Bejelentkezés")
+if authentication_status:
+    st.sidebar.title(f"PlazmaSMS!")
+    authenticator.logout("Kilépés", "sidebar")
 
-        if submit_button:
-            if login(username, password):
-                st.session_state.logged_in = True
-                st.success("Sikeres bejelentkezés")
-                st.experimental_rerun()
-            else:
-                st.error("Hibás felhasználónév vagy jelszó")
-else:
+    st.title("SMS Küldés Vérplazma Donációhoz")
+
+    css = '''
+    <style>
+    [data-testid="stFileUploaderDropzone"] div div::before { content:"Tallózd be az Excel fájlt!"}
+    [data-testid="stFileUploaderDropzone"] div div span{display:none;}
+    [data-testid="stFileUploaderDropzone"] div div::after { content:""}
+    [data-testid="stFileUploaderDropzone"] div div small{display:none;}
+    [data-testid="stFileUploaderDropzone"] button{display:none;}
+    </style>
+    '''
+    st.markdown(css, unsafe_allow_html=True)
+
     # Select message template
     message_template = st.radio(
         "Válassz egy üzenetet",
@@ -134,3 +109,44 @@ else:
                 st.success("Az összes üzenet sikeresen elküldve!")
             else:
                 st.error("Hiba történt az üzenetek küldése közben.")
+
+# Function to verify user login
+def login(username, password):
+    if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
+        return True
+    return False
+
+# Function to extract the second part of the name, handling empty cells
+def get_first_name(full_name):
+    if pd.notna(full_name):
+        parts = full_name.split()
+        if len(parts) > 1:
+            return parts[1]
+    return ""
+
+# Function to convert phone number to required format, handling empty cells
+def convert_phone_number(phone):
+    if pd.notna(phone):
+        phone = str(phone).split('.')[0]  # Remove any decimal points
+        if phone.startswith('06'):
+            return phone.replace('06', '+36', 1)
+        elif phone.startswith('6'):
+            return '+36' + phone[1:]
+    return ""
+
+# Function to format the datetime, handling invalid or empty values
+def format_datetime(dt):
+    try:
+        return pd.to_datetime(dt).strftime('%H:%M')
+    except ValueError:
+        return ""
+
+# Function to construct the curl command with URL encoding
+def construct_curl_command(row, message_template):
+    if message_template == 'donation':
+        message = f"{row['first_name']}, ne felejtsd, hogy a holnapi nap várunk téged vérplazma donációra az OkosPlazmába, {row['formatted_time']}-ra! Mátészalka, Bajcsy-Zsilinszky u. 17."
+    else:
+        message = f"{row['first_name']}, holnap várunk alkalmassági vizsgálatra az OkosPlazmába {row['formatted_time']}-ra, és 5.000 Ft-ot adunk a sikeres vizsgálatért! Bajcsy-Zsilinszky u. 17."
+    
+    encoded_message = urllib.parse.quote(message)
+    return f'curl -s "https://seeme.hu/gateway?key={CURL_KEY}&message={encoded_message}&number={row["formatted_phone"]}&callback=4,6,7&format=json"'
